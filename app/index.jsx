29d7360ba1d3c5b7c9d80d60626bb045c29c061d
Rune <jendralrune@gmail.com>
2025-01-28 23:36:46 +0100

    init

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text} from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text  className='text-3xl font-pblack'>Watcha</Text>
+      <Link href="/home" style={{color: 'blue'}}>Go to Home</Link>
+    </View>
+  );
+}
